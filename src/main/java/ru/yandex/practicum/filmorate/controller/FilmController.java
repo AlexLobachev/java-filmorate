691a4a8ef28c1687dec235b7db51243f81800187
zvezdniy_lord <lelya.praktikum@yandex.ru>
2022-05-12 17:03:37 +0300

    добавлен утилинарный класс validation

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,10 +2,8 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,7 +12,8 @@ import java.util.Map;
 @Slf4j
 public class FilmController {
     private final Map<Integer, Film> films = new HashMap<>();
-    int id = 0;
+    private int id = 0;
+    private final ControllersValidator controllersValidator = new ControllersValidator();
 
     @GetMapping("/films")
     public Collection<Film> getAllFilms() {
@@ -24,29 +23,7 @@ public class FilmController {
 
     @PostMapping(value = "/films")
     public Film addFilm(@RequestBody Film film) {
-
-
-        if (film.getName().isBlank() || film.getName() == null) {
-            throw new ValidationException("Название не может быть пустым");
-        }
-
-            /*for (Film film1 : films.values()) {
-                if (film1.getName().equals(film.getName())) {
-                    throw new ValidationException("Фильм " +
-                            film.getName() + " уже есть в коллекции.");
-                }
-            }*/
-        if (film.getDescription().isBlank() || film.getDescription() == null || film.getDescription().length() > 200) {
-            throw new ValidationException("Описание слишком длинное, макс. длина 200 символов");
-        }
-
-        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
-        }
-
-        if (film.getDuration().getSeconds() < 0) {
-            throw new ValidationException("Продолжительность фильма не может быть отрицательной");
-        }
+        controllersValidator.filmValidator(film);
         film.setId(++id);
         films.put(id, film);
         log.debug("Фильм добавлен, всего фильмов = {}", films.size());
@@ -56,43 +33,9 @@ public class FilmController {
 
     @PutMapping(value = "/films")
     public Film updateFilm(@RequestBody Film film) {
-        // boolean exception = true;
-
-        if (film.getName().isBlank() || film.getName() == null) {
-            throw new ValidationException("Название не может быть пустым");
-        }
-
-        if (film.getDescription().isBlank() || film.getDescription() == null || film.getDescription().length() > 200) {
-            throw new ValidationException("Описание слишком длинное, макс. длина 200 символов");
-        }
-
-        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
-        }
-
-        if (film.getDuration().getSeconds() < 0) {
-            throw new ValidationException("Продолжительность фильма не может быть отрицательной");
-        }
+        controllersValidator.filmValidator(film);
         films.put(film.getId(), film);
-
-            /*for (Film film1 : films.values()) {
-                if (film1.getName().equals(film.getName())) {
-                    film.setOldName(null);
-                    throw new ValidationException("Фильм " +
-                            film.getName() + " уже есть в коллекции.");
-                }
-                if (film1.getName().equals(film.getOldName())) {
-                    film.setId(film1.getId());
-                    films.put(film1.getId(), film);
-                    film.setOldName(null);
-                    exception = false;
-                }
-            }
-            if (exception) {
-                film.setOldName(null);
-                throw new ValidationException("Фильм " +
-                        film.getOldName() + " отсутствует в коллекции.");
-            }*/
+        log.debug("Фильм успешно изменен, всего фильмов = {}", films.size());
 
         return film;
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,10 +2,8 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,7 +12,8 @@ import java.util.Map;
 @Slf4j
 public class UserController {
     private final Map<Integer, User> users = new HashMap<>();
-    int id = 0;
+    private int id = 0;
+    private ControllersValidator controllersValidator = new ControllersValidator();
 
     @GetMapping("/users")
     public Collection<User> getAllUsers() {
@@ -27,29 +26,7 @@ public class UserController {
     public User addUser(@RequestBody User user) {
 
 
-        if (user.getEmail().isBlank() || user.getEmail() == null) {
-            throw new ValidationException("Адрес не может быть пустым");
-        }
-        if (!user.getEmail().contains("@")) {
-            throw new ValidationException("Некорректный Email");
-        }
-            /*for (User user1 : users.values()) {
-                if (user1.getEmail().equals(user.getEmail())) {
-                    throw new ValidationException("Пользователь с электронной почтой " +
-                            user.getEmail() + " уже зарегистрирован.");
-                }
-            }*/
-        if (user.getLogin().isBlank() || user.getLogin() == null || user.getLogin().contains(" ")) {
-            throw new ValidationException("Логин не может быть пустым или содержать пробелы");
-        }
-
-        if (user.getName().isBlank() || user.getName() == null) {
-            user.setName(user.getLogin());
-        }
-
-        if (user.getBirthday().isAfter(LocalDate.now()) || user.getBirthday().equals(LocalDate.now())) {
-            throw new ValidationException("Дата рождения не может быть сегодняшней или будущей");
-        }
+        controllersValidator.userValidator(user);
         user.setId(++id);
         users.put(id, user);
         log.debug("Пользователь добавлен, всего пользователей = {}", users.size());
@@ -60,39 +37,9 @@ public class UserController {
 
     @PutMapping(value = "/users")
     public User updateUser(@RequestBody User user) {
-        //boolean exception = true;
-
-        if (user.getEmail().isBlank() || user.getEmail() == null) {
-            throw new ValidationException("Адрес не может быть пустым");
-        }
-        if (!user.getEmail().contains("@")) {
-            throw new ValidationException("Некорректный Email");
-        }
-
-        if (user.getLogin().isBlank() || user.getLogin() == null || user.getLogin().contains(" ")) {
-            throw new ValidationException("Логин не может быть пустым или содержать пробелы");
-        }
-        if (user.getName().isBlank() || user.getName() == null) {
-            user.setName(user.getLogin());
-        }
-        if (user.getBirthday().isAfter(LocalDate.now()) || user.getBirthday().equals(LocalDate.now())) {
-            throw new ValidationException("Дата рождения не может быть сегодняшней или будущей");
-        }
-
+        controllersValidator.userValidator(user);
         users.put(user.getId(), user);
-
-           /* for (User user1 : users.values()) {
-                if (user1.getEmail().equals(user.getEmail())) {
-                    user.setId(user1.getId());
-                    users.put(user1.getId(), user);
-                    exception = false;
-                }
-            }
-            if (exception) {
-                throw new ValidationException("Пользователь с электронной почтой " +
-                        user.getEmail() + " не зарегистрирован.");
-            }*/
-
+        log.debug("Пользователь успешно изменен, всего фильмов = {}", users.size());
         return user;
     }
 
